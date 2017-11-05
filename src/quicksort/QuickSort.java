98d68fe2dba316231346c3ba8a0be45470503d43
master.mke <master.mke@gmail.com>
2017-11-05 13:02:55 +0200

    optimized to to runs automaticly

--- src/quicksort/QuickSort.java
+++ src/quicksort/QuickSort.java
@@ -2,36 +2,60 @@ package quicksort;
 import java.util.Random;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Arrays;
 
 public class QuickSort {
     public static void main(String args[]){
         
-        int i;
-        int n=100000;
+        int i,r,s;
+        int n=100;
+        int runs=10;
+        int tottime=0;
         int arr[];
+        int timelist[];
         Random randomGenerator = new Random();
         
-        if(args.length != 0)
+        timelist= new int[10];
+        
+        if(args.length == 1){
             n=Integer.parseInt(args[0]);
-
-        arr= new int[n];
-
-        for(i=0;i<n;i++){
-            arr[i] = randomGenerator.nextInt(n);
         }
 
-        System.out.println("Array generated...");
-        
-        System.out.println("Sorting array...");
-        Instant start = Instant.now();
-        quickSort(arr, 0, n-1);
-        Instant end = Instant.now();
-        Duration timeElapsed = Duration.between(start, end);
+        if(args.length == 2){
+            n=Integer.parseInt(args[0]);
+            runs=Integer.parseInt(args[1]);
+        }       
 
-        System.out.println("Array sorted!");
-        System.out.println("Array size: "+ arr.length +" elements");
-        System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds");
+        for(s=1;s<=5;s++){
+            arr= new int[n];
 
+            for(r=1;r<=runs;r++){
+                System.out.println("Starting run no."+r);
+                System.out.println("Generating array for run no."+r);
+                for(i=0;i<n;i++){
+                    arr[i] = randomGenerator.nextInt(n);
+                }
+
+                System.out.println("Array generated...");
+
+                System.out.println("Sorting array...");
+
+                Instant start = Instant.now();
+                quickSort(arr, 0, n-1);
+                Instant end = Instant.now();
+                Duration timeElapsed = Duration.between(start, end);
+
+                System.out.println("Array sorted...");
+                System.out.println("Array size: "+ arr.length +" elements");
+                System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds in run no."+r);
+                tottime += timeElapsed.toMillis();
+            }
+
+            System.out.println("Average time taken for  "+ runs +" runs = "+ tottime/runs +" milliseconds");
+            n=n*10;
+            timelist[s]=tottime/runs;
+        }
+        System.out.println("Average time taken for  all runs:\r\n "+Arrays.toString(timelist));
     }
 
     static int partition(int arr[], int left, int right){
